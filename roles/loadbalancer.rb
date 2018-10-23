4d8367ba77b2c9ee57072d99c2d9b588ab5d207f
Mark Leybourne <markleybourne@macbook-pro.local>
2018-10-23 15:30:57 +0100

    Share role

--- roles/loadbalancer.rb
+++ roles/loadbalancer.rb
@@ -1,6 +1,7 @@
 name 'loadbalancer'
 description 'load balancer'
 run_list 'recipe[haproxy]'
+run_list 'role[base]', 'recipe[haproxy]'
 default_attributes({
     "apache" => {
         "port" => 8181
--- roles/web.rb
+++ roles/web.rb
@@ -1,6 +1,7 @@
 name 'web'
 description 'Web Server'
 run_list 'recipe[apache]'
+run_list 'role[base]', 'recipe[haproxy]'
 default_attributes({
     "apache" => {
         "port" => 8181
