3d9fd08a516d7d9bc138e62f4ebdbc5d47501275
Mark Leybourne <markleybourne@macbook-pro.local>
2018-10-23 12:00:24 +0100

    Use attribute

--- cookbooks/haproxy/metadata.rb
+++ cookbooks/haproxy/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures haproxy'
 long_description 'Installs/Configures haproxy'
-version '0.2.0'
+version '0.3.0'
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -18,3 +18,6 @@ chef_version '>= 13.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/haproxy'
+
+# needed for attribute defined in apache cookbook attributes
+depends 'apache', '>= 0.3.1'
