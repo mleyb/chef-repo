81967cdef31561d2a197f7cf854b8d8da52b80a5
Mark Leybourne <markleybourne@macbook-pro.local>
2018-10-23 10:53:20 +0100

    Implement template logic

--- cookbooks/haproxy/recipes/default.rb
+++ cookbooks/haproxy/recipes/default.rb
@@ -11,7 +11,7 @@ allwebservers = search('node', 'recipes:apache\:\:default')
 template '/etc/haproxy/haproxy.cfg' do
   source 'haproxy.cfg.erb'
   variables(
-      :webservers = allwebservers
+    :webservers => allwebservers
   )
   notifies :restart, 'service[haproxy]'
 end
